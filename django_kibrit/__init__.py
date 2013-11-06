3a94b16427c4b78810f504e723bed426cb164b68
sinanm89 <sinanm89@gmail.com>
2013-11-06 16:55:58 +0200

    revised documentation

--- django_kibrit/__init__.py
+++ django_kibrit/__init__.py
@@ -9,21 +9,7 @@ Use it for control a static's versions.
 
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __project__ = __name__
 __author__ = "Sinan Midillili <sinan@hipo.biz>"
 __license__ = "BSD"
-
-
-def get_backend(name, **kwargs):
-    """
-
-    Create and return backend
-
-    """
-
-    from importlib import import_module
-
-    mod = import_module(__name__ + '.' + name)
-    import pdb;pdb.set_trace()
-    return mod.Backend(**kwargs)
